b7be8a5b7db2c05881e431a5a369af016ca8f0e5
yunzae <dsa05037@naver.com>
2023-04-04 11:06:05 +0900

    60min, 반례힌트참고함

---
+++ 이진탐색/BOJ2343 기타 레슨.py
@@ -0,0 +1,40 @@
+# 9:55 실버1
+
+import sys
+
+N,M =map(int,sys.stdin.readline().split())
+bluelay_list=list(map(int,sys.stdin.readline().split()))
+
+def greedy(time,video_list):
+    sum=0
+    count=1
+    for video in video_list:
+        if video+sum <=time:
+            sum+=video
+        else:
+            sum=video
+            count+=1
+    return(count)
+
+def bineary():
+    start=min(bluelay_list)
+    end=sum(bluelay_list)
+    min_time=max(bluelay_list)
+    time=end
+    while start<=end:
+        mid=(start+end)//2
+        need_disk= greedy(mid, bluelay_list)
+        print("start: ", start,"end: ", end,"mid: ",mid,"need: ",need_disk)
+
+        if mid<time and mid>=min_time:
+            if need_disk<=M:
+                time = mid
+                end = mid - 1
+            else:
+                start=mid+1
+        else:
+            start=mid+1
+
+    return time
+print(bineary())
+
